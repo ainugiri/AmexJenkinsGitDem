cf8f10fb78f509de4f0e293ea4b8baf503fedafd
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-07-14 15:38:54 +0530

    Update Sample wrt Changes.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 class Sample{
 
 public static void main(String[] arg){
-System.out.println("TEST");
+System.out.println("CHANGES");
 System.out.println("Push the code from GITHUB with help webhook");
 System.out.println("ngrok url is mapped to out local host");
 System.out.println("Job is triggered");
