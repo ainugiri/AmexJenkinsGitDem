b4792950730f0b193b9997d5f50c5970c285f397
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-07-14 15:35:06 +0530

    Update Sample.java
    Test010101

--- Sample.java
+++ Sample.java
@@ -2,10 +2,8 @@ class Sample{
 
 public static void main(String[] arg){
 System.out.println("TEST");
-System.out.println("GITHUB File2 Executed in Jenkins Server");
-System.out.println("GITHUB File2 Executed in Jenkins Server");
-System.out.println("GITHUB File2 Executed in Jenkins Server");
-System.out.println("GITHUB File2 Executed in Jenkins Server");
-System.out.println("GITHUB File2 Executed in Jenkins Server");
+System.out.println("Push the code from GITHUB with help webhook");
+System.out.println("ngrok url is mapped to out local host");
+System.out.println("Job is triggered");
 }
 }
