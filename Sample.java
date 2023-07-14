604fb0575e107efde7f771be2c507eb107668e69
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-07-14 14:38:46 +0530

    Update Sample.java
    Test

--- Sample.java
+++ Sample.java
@@ -3,5 +3,9 @@ class Sample{
 public static void main(String[] arg){
 System.out.println("GITHUB File Executed in Jenkins Server");
 System.out.println("GITHUB File2 Executed in Jenkins Server");
+System.out.println("GITHUB File2 Executed in Jenkins Server");
+System.out.println("GITHUB File2 Executed in Jenkins Server");
+System.out.println("GITHUB File2 Executed in Jenkins Server");
+System.out.println("GITHUB File2 Executed in Jenkins Server");
 }
 }
