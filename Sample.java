8f9bc7c903621ff2870eb4330d804bed6c5d7075
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-07-14 15:41:08 +0530

    Update Sample.java
    test

--- Sample.java
+++ Sample.java
@@ -5,5 +5,6 @@ System.out.println("CHANGES");
 System.out.println("Push the code from GITHUB with help webhook");
 System.out.println("ngrok url is mapped to out local host");
 System.out.println("Job is triggered");
+System.out.println("Job is triggered");
 }
 }
